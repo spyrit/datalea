30497f83e3edeb456a09bafe9774d7edef87b583
Charles SANQUER <charles.sanquer@spyrit.net>
2013-02-08 18:32:20 +0100

    change error reporting in prod index

--- web/index.php
+++ web/index.php
@@ -5,6 +5,6 @@ define('SILEX_ENV', 'prod');
 define('SILEX_DEBUG', false);
 
 ini_set('display_errors', 'stderr');
-//error_reporting(E_ERROR | E_CORE_ERROR| E_USER_ERROR);
+error_reporting(E_ERROR | E_CORE_ERROR| E_USER_ERROR);
 
 include_once __DIR__.DS.'..'.DS.'app'.DS.'app.php';
