c53f9002352ff7972d3f5ce667860fe04d6cfa11
Charles SANQUER <charles.sanquer@spyrit.net>
2013-02-08 17:09:40 +0100

    remove unused ip

--- web/index_dev.php
+++ web/index_dev.php
@@ -4,7 +4,6 @@
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-    '109.190.4.222', //Spyrit office
 ))) {
     die('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
