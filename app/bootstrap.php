4a624cbc3577d966af7a8212bfd5c81ea650d30f
Charles SANQUER <charles.sanquer@spyrit.net>
2013-02-15 18:19:19 +0100

    enable YAML translations files

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -25,6 +25,7 @@ use \Symfony\Component\HttpKernel\Exception\HttpException;
 use \Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use \Symfony\Component\Yaml\Exception\ParseException;
 use \Symfony\Component\Yaml\Yaml;
+use \Symfony\Component\Translation\Loader\YamlFileLoader;
 
 // get environment constants or set default
 if (!defined('DS')) {
@@ -160,6 +161,18 @@ function createDefaultSilexApp($appdir = __DIR__, $env = 'prod', $debug = false)
         'locale_fallback' => empty($config['locale_fallback']) ? 'en' : $config['locale_fallback'],
     ));
 
+    $app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
+        $translator->addLoader('yaml', new YamlFileLoader());
+        $languages = array('en', 'fr');
+        $domains = array('messages');
+        foreach ($languages as $lang) {
+            foreach ($domains as $domain) {
+                $translator->addResource('yaml', $app['app_dir'].DS.'translations'.DS.$domain.'.'.$lang.'.yml', $lang);
+            }
+        }
+        return $translator;
+    }));
+    
     // add twig templating
     $app->register(new TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/views',
